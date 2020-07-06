e81fb81e636c098a582891234b153a0c60c2188b
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2020-07-06 13:08:15 +0300

    внесены замечания по проверке

--- js/script.js
+++ js/script.js
@@ -1,112 +1,111 @@
 // Форма регистрации в шапке
 (function () {
-const loginLink = document.querySelector(".login-link");
-const loginPopup = document.querySelector(".user-entrance");
-const loginForm = loginPopup.querySelector(".entrance-form");
-const loginPassword = loginPopup.querySelector("#entrance-password");
-const loginLogin = loginPopup.querySelector("#entrance-email");
+  const loginLink = document.querySelector(".login-link");
+  const loginPopup = document.querySelector(".user-entrance");
+  const loginForm = loginPopup.querySelector(".entrance-form");
+  const loginPassword = loginPopup.querySelector("#entrance-password");
+  const loginLogin = loginPopup.querySelector("#entrance-email");
 
-const isStorageSupport = true;
-let storage = "";
-try {
-  storage = localStorage.getItem("login");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-loginLink.addEventListener("mouseover", function (evt) {
-  evt.preventDefault();
-  loginPopup.classList.add("show");
-  if (storage) {
-    loginLogin.value = storage;
-    loginPassword.focus();
-  } else {
-    loginLogin.focus();
+  const isStorageSupport = true;
+  let storage = "";
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
   }
-});
 
-loginForm.addEventListener("submit", function (evt) {
-  if (!loginLogin.value || !loginPassword.value) {
+  loginLink.addEventListener("mouseover", function (evt) {
     evt.preventDefault();
-    loginPopup.classList.remove("modal-error");
-    loginPopup.offsetWidth = loginPopup.offsetWidth;
-    loginPopup.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", loginLogin.value);
+    loginPopup.classList.add("show");
+    if (storage) {
+      loginLogin.value = storage;
+      loginPassword.focus();
+    } else {
+      loginLogin.focus();
     }
-  }
-});
-})()
+  });
+
+  loginForm.addEventListener("submit", function (evt) {
+    if (!loginLogin.value || !loginPassword.value) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-error");
+      loginPopup.offsetWidth = loginPopup.offsetWidth;
+      loginPopup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", loginLogin.value);
+      }
+    }
+  });
+})();
 
 //  Модальное окно (форма обратной связи)
-function modal () {
-const body = document.querySelector(".page-body");
-const feedback = body.querySelector(".popup");
-const feedbackForm = feedback.querySelector(".popup-form");
-const feedbackBtn = body.querySelector(".feedback-btn");
-const feedbackClose = feedback.querySelector(".popup-close");
-const questionName = feedbackForm.querySelector("#question-name");
-const questionEmail = feedbackForm.querySelector("#question-email");
-const questionText = feedbackForm.querySelector("#question-text");
-const feedbackFormBtn = feedbackForm.querySelector(".button-form");
+(function () {
+  const body = document.body;
+  const feedback = body.querySelector(".popup");
+  const feedbackForm = feedback.querySelector(".popup-form");
+  const feedbackBtn = body.querySelector(".feedback-btn");
+  const feedbackClose = feedback.querySelector(".popup-close");
+  const questionName = feedbackForm.querySelector("#question-name");
+  const questionEmail = feedbackForm.querySelector("#question-email");
+  const questionText = feedbackForm.querySelector("#question-text");
+  const feedbackFormBtn = feedbackForm.querySelector(".button-form");
 
-const isStorageSupport = true;
-let storage = "";
-try {
-  storage = localStorage.getItem("name");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-feedbackBtn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedback.classList.add("popup-show");
-  body.classList.add("overlay");
-  if (storage) {
-    questionName.value = storage;
-    questionEmail.value = storage;
-    questionText.focus();
-  } else {
-    questionName.focus();
+  const isStorageSupport = true;
+  let storage = "";
+  try {
+    storage = localStorage.getItem("name");
+  } catch (err) {
+    isStorageSupport = false;
   }
-});
 
-feedbackClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedback.classList.remove("popup-show");
-  body.classList.remove("overlay");
-});
+  feedbackBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedback.classList.add("popup-show");
+    body.classList.add("overlay");
+    if (storage) {
+      questionName.value = storage;
+      questionEmail.value = storage;
+      questionText.focus();
+    } else {
+      questionName.focus();
+    }
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (feedback.classList.contains("popup-show")) {
+  feedbackClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedback.classList.remove("popup-show");
+    body.classList.remove("overlay");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (feedback.classList.contains("popup-show")) {
+        evt.preventDefault();
+        feedback.classList.remove("popup-show");
+        body.classList.remove("overlay");
+      }
+    }
+  });
+
+  feedbackFormBtn.addEventListener("click", function (evt) {
+    if (!questionName.value || !questionEmail.value) {
       evt.preventDefault();
-      feedback.classList.remove("popup-show");
-      body.classList.remove("overlay");
+      feedbackForm.classList.remove("modal-error");
+      feedbackForm.offsetWidth = feedbackForm.offsetWidth;
+      feedbackForm.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("name", questionName.value);
+        localStorage.setItem("email", questionEmail.value);
+      }
     }
-  }
-});
-
-feedbackFormBtn.addEventListener("click", function (evt){
-  if (!questionName.value || !questionEmail.value) {
-    evt.preventDefault(); 
-    feedbackForm.classList.remove("modal-error");
-    feedbackForm.offsetWidth = feedbackForm.offsetWidth;
-    feedbackForm.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("name", questionName.value);
-      localStorage.setItem("email", questionEmail.value);
-    }
-}
-});}
-modal();
-// не смогла найти какой-то глюк. Когда я заключаю функцию обратной связи в самозапускающуюся функцию, всплывает какая-то ошибка. Пробовала раз 10 уже. Скорее всего есть какая-то синтаксическая ошибка, которую я не могу найти. Поэтому сделала пока такой вариант (временно).
+  });
+})();
 
 // Главный слайдер
 (function () {
-  const body = document.querySelector(".page-body");
+  const body = document.body;
   const sliderSwitches = body.querySelectorAll(".main-slider-switch");
 
   for (let i = 0; i < sliderSwitches.length; i++) {
@@ -127,16 +126,15 @@ modal();
 // Карта
 ymaps.ready(function () {
   const myMap = new ymaps.Map('map', {
-      center: [59.9398806677677,30.33193647360036],
+      center: [59.9398806677677, 30.33193647360036],
       zoom: 16,
       controls: []
-    },{
+    }, {
       suppressMapOpenBlock: true
-    },{
+    }, {
       searchControlProvider: 'yandex#search'
     }),
-    myPlacemark = new ymaps.Placemark(([59.94077427553936,30.325949783079135]), {
-    }, {
+    myPlacemark = new ymaps.Placemark(([59.94077427553936, 30.325949783079135]), {}, {
       iconLayout: 'default#image',
       iconImageHref: 'img/pin.svg',
       iconImageSize: [218, 142],
